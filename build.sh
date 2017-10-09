593fde1cc76bb043e5c80c60d0ef77eb6d480247
vasil <vasil.sokolov@gmail.com>
2017-10-09 15:20:25 +0300

    update

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 npm install -g gulp-cli
-#npm install -g newman
 npm install ./modules/CoreWebclient
 
 gulp styles --themes Default,DeepForest,Funny
-#gulp js:build
-#gulp js:min
+gulp js:build
+gulp js:min
+gulp test
 
-
-
-
-#newman run tests/bitcoinz.postman_collection.json -e tests/tests.postman_environment.json
