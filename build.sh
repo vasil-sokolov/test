f3980fdd9ef9b54278d6f7fa355553f21548e58f
vasil <vasil.sokolov@gmail.com>
2017-10-16 14:33:31 +0300

    fix build issue

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ gulp js:build
 gulp js:min
 gulp test
 
+zip -r package.zip data/settings/modules modules static system vendor  ".htaccess" dav.php common.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak
