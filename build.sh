e9b19bc2555f031f4f576cb759b7ba92be490aa2
vasil <vasil.sokolov@gmail.com>
2017-11-01 10:39:42 +0300

    test config

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-npm install -g gulp-cli
-npm install ./modules/CoreWebclient
+# npm install -g gulp-cli
+# npm install ./modules/CoreWebclient
 
-gulp styles --themes Default,DeepForest,Funny
-gulp js:build
-gulp js:min
-gulp test
+# gulp styles --themes Default,DeepForest,Funny
+# gulp js:build
+# gulp js:min
+# gulp test
 
 if [ "$1" != "" ]; then
 	zip -r $1.zip favicon.ico robots.txt composer.json modules.json
